16d82c2bb9dbcf0474f1f7b9f4a60a27cb4fc513
bigQY <1462459528@qq.com>
2021-10-26 15:33:42 +0800

    主页面重构，使用page来显示主要内容

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,6 +26,7 @@ using System.Windows.Media;
 using System.Configuration;
 using FunyCamNF.utils;
 using AForge.Video.FFMPEG;
+using FunyCamNF.pages.main;
 
 namespace FunyCamNF
 {
@@ -34,163 +35,14 @@ namespace FunyCamNF
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<string> deviceList = new List<string>();
-        private FilterInfoCollection videoDevices;
-        private VideoCaptureDevice originalSource;
-        private AsyncVideoSource transSource;
-
-        private List<string> filterList = new List<string>();
-
-        private int selectedIndex = 0;
-        private VideoFileWriter writer;   
-        private bool IsRecordVideo = false;   //是否开始录像
-
+        MainPage mainPage = new MainPage();
         public MainWindow()
         {
             InitializeComponent();
-            filterList.Add("反色");
-            filterList.Add("模糊");
-            filterList.Add("edges");
-            filterList.Add("pixellate");
-            filterList.Add("Jitter");
-            filterList.Add("Erosion3x3");
-            filterList.Add("动态旋转");
-            filterListBox.ItemsSource = filterList;
-            getDevices();
-
-            
-            
+            mainFrame.Content = mainPage;
         }
 
-        private void getDevices()
-        {
-            try
-            {
-                // 枚举所有视频输入设备
-                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
-                if (videoDevices.Count == 0)
-                    throw new ApplicationException();
-
-                foreach (FilterInfo device in videoDevices)
-                {
-                    deviceList.Add(device.Name);
-                }
-                camListBox.ItemsSource = deviceList;
-                camListBox.SelectedIndex = 0;
-                filterListBox.SelectedItem = 0;
-                // 检查上次选择的相机设备
-                string lastDeviceName = Tools.readSettings("lastDeviceName");
-                for(int i = 0; i < deviceList.Count; i++)
-                {
-                    if (deviceList[i].Equals(lastDeviceName))
-                    {
-                        camListBox.SelectedIndex = i;
-                        break;
-                    }
-                }
-                //检查上次的滤镜选择
-                string lastFilterName = Tools.readSettings("lastFilterName");
-                for(int i=0;i< filterList.Count; i++)
-                {
-                    if (filterList[i].Equals(lastFilterName))
-                    {
-                        filterListBox.SelectedIndex = i;
-                    }
-                }
-
-
-            }
-            catch (ApplicationException)
-            {
-                camListBox.Items.Add("No local capture devices");
-                videoDevices = null;
-            }
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Button_Click_1(sender, e);
-            originalSource = new VideoCaptureDevice(videoDevices[camListBox.SelectedIndex].MonikerString);//连接摄像头
-            originalSource.VideoResolution = originalSource.VideoCapabilities[camListBox.SelectedIndex];
-
-
-            transSource = new AsyncVideoSource(originalSource);
-            transSource.NewFrame += OriginalSource_NewFrame;
-            transSource.Start();
-
-            vp1.VideoSource = transSource;
-            vp2.VideoSource = originalSource;
-            vp2.Start();
-            vp1.Start();
-            //保存当前相机设备名
-            Tools.saveSettings("lastDeviceName", deviceList[camListBox.SelectedIndex]);
-            //保存当前滤镜名
-            Tools.saveSettings("lastFilterName", filterList[filterListBox.SelectedIndex]);
-        }
-
-        private void OriginalSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
-        {
-            Bitmap bitmap = eventArgs.Frame;
-            if (selectedIndex == 0)
-            {
-                Invert filter = new Invert();
-                filter.ApplyInPlace(bitmap);
-            }
-            else if (selectedIndex == 1)
-            {
-                Blur filter = new Blur();
-                filter.ApplyInPlace(bitmap);
-            }
-            else if (selectedIndex == 2)
-            {
-                Edges filter = new Edges();
-                filter.ApplyInPlace(bitmap);
-            }
-            else if (selectedIndex == 3)
-            {
-                AForge.Imaging.Filters.Pixellate pixellate = new Pixellate(50);
-                pixellate.ApplyInPlace(bitmap);
-            }
-            else if (selectedIndex == 4)
-            {
-                Jitter pointed = new Jitter(500);
-                pointed.ApplyInPlace(bitmap);
-            }
-            else if (selectedIndex == 5)
-            {
-                TestFilter testFilter = new TestFilter();
-                testFilter.ApplyInPlace(bitmap);
-            }
-            else if (selectedIndex == 6)
-            {
-                DynamicRotateFilter dynamicRotateFilter = new DynamicRotateFilter();
-                dynamicRotateFilter.ApplyInPlace(bitmap);
-            }
-
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            vp2.WaitForStop();
-            vp1.WaitForStop();
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            Button_Click_1(sender, e);
-            Button_Click(sender, e);
-        }
-
-        private void filterListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            selectedIndex = filterListBox.SelectedIndex;
-        }
-
-        private void btnSetting_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
+     
        
 
 
@@ -206,8 +58,9 @@ namespace FunyCamNF
             }
 
             MenuToggleButton.IsChecked = false;
-            formHost1.Visibility = Visibility.Visible;
-            formHost2.Visibility = Visibility.Visible;
+            
+            mainPage.formsHostFiltered.Visibility = Visibility.Visible;
+            mainPage.formsHostOrigin.Visibility = Visibility.Visible;
 
         }
 
@@ -256,8 +109,8 @@ namespace FunyCamNF
 
         private void hideForm()
         {
-            formHost1.Visibility = MenuToggleButton.IsChecked.Value? Visibility.Hidden : Visibility.Visible;
-            formHost2.Visibility = MenuToggleButton.IsChecked.Value ? Visibility.Hidden : Visibility.Visible;
+             mainPage.formsHostFiltered.Visibility = MenuToggleButton.IsChecked.Value? Visibility.Hidden : Visibility.Visible;
+             mainPage.formsHostOrigin.Visibility = MenuToggleButton.IsChecked.Value ? Visibility.Hidden : Visibility.Visible;
         }
 
         private void ToggleButton_Click(object sender, RoutedEventArgs e)
@@ -286,4 +139,4 @@ namespace FunyCamNF
 
     }
 }
-}
+
