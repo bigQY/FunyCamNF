5adcf19c4a31339f5865c4a25d013566798e751f
bigQY <1462459528@qq.com>
2021-12-14 17:39:16 +0800

    fix Multithread

--- filters/ConvexFilterMultithread.cs
+++ filters/ConvexFilterMultithread.cs
@@ -10,12 +10,17 @@ using System.Threading.Tasks;
 
 namespace FunyCamNF.filters
 {
-    class ConvexFilterMultithread : BaseInPlacePartialFilter
+    unsafe class ConvexFilterMultithread : BaseInPlacePartialFilter
     {
 
         private static readonly object Lock = new object();
 
         byte[] tempDataByte = null;
+
+        int centerX;
+        int centerY;
+
+        byte* srcData;
 
         public Dictionary<PixelFormat, PixelFormat> formatTranslations = new Dictionary<PixelFormat, PixelFormat>();
 
@@ -45,30 +50,26 @@ namespace FunyCamNF.filters
         protected override unsafe void ProcessFilter(UnmanagedImage image, Rectangle rect)
         {
             int pixelSize = ((image.PixelFormat == PixelFormat.Format8bppIndexed) || (image.PixelFormat == PixelFormat.Format16bppGrayScale) ? 1 : 3);
-            int startY = rect.Top;
-            int stopY = startY + rect.Height;
-            int startX = rect.Left * pixelSize;
-            int stopX = startX + rect.Width * pixelSize;
             int srcLength = rect.Width * rect.Height * pixelSize;
             byte* basePtr = (byte*)image.ImageData.ToPointer();
+            srcData = basePtr;
             tempDataByte = new byte[srcLength];
+            centerX = rect.Width / 2;
+            centerY = rect.Height / 2;
             Task[] tasks = new Task[4]
             {
-                Task.Factory.StartNew(()=>pixelProcess(basePtr,0,0,rect.Width/2,rect.Height/2)),
-                Task.Factory.StartNew(()=>pixelProcess(basePtr,rect.Width/2,0,rect.Width,rect.Height/2)),
-                Task.Factory.StartNew(()=>pixelProcess(basePtr,0,rect.Height/2,rect.Width/2,rect.Height)),
-                Task.Factory.StartNew(()=>pixelProcess(basePtr,rect.Width/2,rect.Height/2,rect.Width,rect.Height)),
+                Task.Factory.StartNew(()=>pixelProcess(0,0,centerX,centerY)),
+                Task.Factory.StartNew(()=>pixelProcess(centerX,0,rect.Width,centerY)),
+                Task.Factory.StartNew(()=>pixelProcess(0,centerY,centerX,rect.Height)),
+                Task.Factory.StartNew(()=>pixelProcess(centerX,centerY,rect.Width,rect.Height)),
             };
             Task.WaitAll(tasks);
             WriteBytesToPtr(image.ImageData, tempDataByte);
         }
 
-        private unsafe void pixelProcess(byte* srcData, int startW,int startH,int endW, int endH)
+        private unsafe void pixelProcess( int startW,int startH,int endW, int endH)
         {
-            int oldX = 0;
-            int oldY = 0;
-            int centerX = endW -startW;
-            int centerY = endH -startH;
+            
             int length=tempDataByte.Length;
             //扭曲区间
             int R;
@@ -77,27 +78,25 @@ namespace FunyCamNF.filters
                 R = (int)(Math.Sqrt(4 * centerX * centerX + 4 * centerY * centerY) / 2);
             }
 
-            for (int j = 0; j < endH; j++)
+            for (int j = startH; j < endH; j++)
             {
-                for (int i = 0; i < endW; i++)
+                for (int i = startW; i < endW; i++)
                 {
-                    oldX = i;
-                    oldY = j;
                     double dis;
                     lock (Lock)
                     {
                         dis = Math.Sqrt((centerX - i) * (centerX - i) + (centerY - j) * (centerY - j));
                     }
 
-                    int newX = (int)((oldX - centerX) * dis / R + centerX);
-                    int newY = (int)((oldY - centerY) * dis / R + centerY);
+                    int newX = (int)((i - centerX) * dis / R + centerX);
+                    int newY = (int)((j - centerY) * dis / R + centerY);
 
-                    int tempB = i * 3 + j * endW * 3;
-                    int tempG = i * 3 + j * endW * 3 + 1;
-                    int tempR = i * 3 + j * endW * 3 + 2;
-                    int srcB = newX * 3 + newY * endW * 3;
-                    int srcG = newX * 3 + newY * endW * 3 + 1;
-                    int srcR = newX * 3 + newY * endW * 3 + 2;
+                    int tempB = i * 3 + j * centerX*2 * 3;
+                    int tempG = i * 3 + j * centerX * 2 * 3 + 1;
+                    int tempR = i * 3 + j * centerX * 2 * 3 + 2;
+                    int srcB = newX * 3 + newY * centerX * 2 * 3;
+                    int srcG = newX * 3 + newY * centerX * 2 * 3 + 1;
+                    int srcR = newX * 3 + newY * centerX * 2 * 3 + 2;
                     if ((tempB <= length) && (tempG <= length) && (tempR <= length) && (srcB <= length) && (srcG <= length) && (srcR <= length))
                     {
                         Monitor.Enter(Lock);
